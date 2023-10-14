15c327d3626fbf792bd2c4e00352b68423afb81a
Rohit <102830723+p-rohitt@users.noreply.github.com>
2023-10-14 18:59:34 +0530

    fixed form css

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
-import Form2 from "../components/Form2";
+import Popup from "../components/Popup";
 import Form1 from "../components/Form1";
+import Form2 from "../components/Form2";
+
 
 function Form() {
   const [formType, setFormType] = useState(0);
@@ -58,63 +59,17 @@ function Form() {
     employment_type: "",
   });
 
-  return (
-    <div className="min-h-screen overflow-hidden flex justify-center items-center bg-gradient-to-br from-emerald-700 to-sky-700">
-      <div className="bg-white h-[40rem] w-[65rem] rounded-xl flex flex-col items-center overflow-y-scroll overflow-x-hidden">
-        <div className="flex mt-2 justify-end w-full mr-4 items-center mb-10">
-          {/* <h2 className="font-bold font-inter p-2 text-slate-700 text-3xl uppercase tracking-widest">
-            Risk Evaluation Form
-          </h2> */}
-          {formType != 0 && (
-            <div className="flex justify-between px-1 w-full">
-              <img
-                src="../public/tvs.png"
-                alt=""
-                className="object-contain h-10 w-64"
-              />
-              <button
-                onClick={() => setFormType(0)}
-                className="h-10 bg-emerald-500 rounded-full w-24 font-bold font-inter text-white"
-              >
-                Back
-              </button>
-            </div>
-          )}
-        </div>
 
-        {formType == 0 && (
-          <div className="flex flex-col justify-center items-center mt-56">
-            <div className="text-3xl font-inter font-bold">
-              Are you an existing customer of TVS?
-            </div>
-            <div className="flex space-x-10 mt-10">
-              <button
-                onClick={() => setFormType(1)}
-                className="bg-green-500 text-white h-10 w-44 rounded-full font-inter font-semibold"
-              >
-                YES
-              </button>
-              <button
-                onClick={() => setFormType(2)}
-                className="bg-red-500 text-white h-10 w-44 rounded-full font-inter font-semibold"
-              >
-                NO
-              </button>
-            </div>
-          </div>
-        )}
-        {formType === 1 && (
-          <div className="">
-            <Form1 formData={formData} setFormData={setFormData} />
-          </div>
-        )}
-        {formType === 2 && (
-          <div className="">
-            <Form2 secondForm={secondForm} setSecondForm={setSecondForm} />
-          </div>
-        )}
-      </div>
-    </div>
+  return (
+
+    <>
+      {formType === 0 && <Popup formType={formType } setFormType={setFormType} />}
+      {formType === 1 && <Form1 formType={formType} setFormType={setFormType} formData={formData} setFormData={setFormData} />}
+      {formType === 2 && <Form2 formType={formType} setFormType={setFormType} secondForm={secondForm} setSecondForm={setSecondForm} />}
+      
+    
+    </>
+   
   );
 }
 
--- src/Result.jsx
+++ src/Result.jsx
@@ -7,29 +7,50 @@ import Speedometer from "../components/Speedometer";
 
 function Result() {
     
-     const searchParams = new URLSearchParams(window.location.search);
-     const result = searchParams.get("result");  
+    const searchParams = new URLSearchParams(window.location.search);
+    const result = searchParams.get("result");  
+    const interestRate = searchParams.get("interestRate");
   
     console.log(result)
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-emerald-700 to-sky-700 ">
-            <div className="bg-white h-[40rem] w-[65rem] rounded-xl flex flex-col items-center overflow-y-scroll">
-                <div className = "flex justify-end px-2 w-full">
-                    <img src="../public/tvs.png" alt="" className="object-contain h-20 w-64" />
-                </div>
-                <div className = "mt-10">
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-emerald-700 to-sky-700 ">
+        <div className="bg-white h-[40rem] w-[65rem] rounded-xl flex flex-col items-center overflow-y-scroll">
+          <div className="flex justify-end px-2 w-full">
+            <img
+              src="../public/tvs.png"
+              alt=""
+              className="object-contain h-20 w-64"
+            />
+          </div>
+          <div className="mt-10">
+            <Speedometer speed={result} />
+          </div>
+          <div>
+            {result <= 40 && (
+              <h2 className="font-inter font-bold text-2xl tracking-wide">
+                We are pleased to share the results of our credit risk
+                assessment. Based on our analysis, you exhibit a very low credit
+                risk, indicating your eligibility for a loan. Should you choose
+                to apply, you can be considered for a loan with the favorable
+                standard 6% interest rate. We appreciate your responsible
+                financial behavior and welcome any inquiries or applications you
+                may have.
+              </h2>
+            )}
 
-                <Speedometer speed={result} />
-                </div>
-                <div>
+            {result > 40 && result < 70 && (
+              <h2 className="font-inter font-bold text-3xl tracking-wide">
+                Additional interest
+              </h2>
+            )}
 
-                {result <= 40 && <h2 className = "font-inter font-bold text-3xl tracking-wide">Congratulations! You are eligible for the loan.</h2>}
-                
-                {result > 40 && result < 70 && <h2 className = "font-inter font-bold text-3xl tracking-wide">Additional interest</h2>}
-                
-                {result>=70 &&  <h2 className = "font-inter font-bold tracking-wide text-2xl">You incur a high risk percentage. please consult a bank!</h2>}
-                </div>
+            {result >= 70 && (
+              <h2 className="font-inter font-bold tracking-wide text-2xl">
+                You incur a high risk percentage. please consult a bank!
+              </h2>
+            )}
+          </div>
         </div>
       </div>
     );
